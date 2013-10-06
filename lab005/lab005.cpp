99c3a5e3a9e914edd21760b7131853f35fda863b
Francis Kim <kims_0121@naver.com>
2013-10-06 14:30:49 +0900

    lab005 완료

--- lab005/lab005.cpp
+++ lab005/lab005.cpp
@@ -1,49 +1,65 @@
 // Lab 005
-// Name :
-// Student ID :
+// Name : ±è¼º±Ù	
+// Student ID : 20093267
 
 #include "lab005.h"
 
 template <class T>
-void CircularList<T>::InsertFront(const T & e)
-{// Insert the element e at the front position
-
-
+void CircularList<T>::InsertFront(const T & e){
+// Insert the element e at the front position
+	head->link = new ChainNode<T>(e, head->link);
 }
 
 
 template <class T>
-bool CircularList<T>::Delete(const T & e)
-{
-	// delete an element from the position theIndex
+bool CircularList<T>::Delete(const T & e){ // delete an element from the position theIndex
+	ChainNode<T>* currentNode = head->link;
 
+	int index = 0;
+	while(currentNode != NULL && currentNode->data != e){
+		currentNode = currentNode->link;
+		index++;
+	}
 
-
-
-
-
-	return false;
+	if(currentNode == NULL){
+		return false;
+	}else{
+		if(head->link == NULL){
+			return false;
+		}
+		ChainNode<T>* deleteNode;
+		if(index == 0){
+			deleteNode = head->link;
+			head->link = head->link->link;
+		}else{
+			ChainNode<T>* p = head->link;
+			for(int i=0; i<index-1;i++){
+				if(p == 0)
+					return false;
+				p = p->link;
+			}
+			if( (p == 0) || (p->link ==0) )
+				return false;
+			deleteNode = p->link;
+			p->link = p->link->link;
+		}
+		delete deleteNode;
+		return true;
+	}
 }
 
-
-
 template <class T>
-int  CircularList<T>::Size() const 
-{
+int  CircularList<T>::Size() const {
 	int cnt = 0;
 
-
-
-
-
+	ChainNode<T>* currentNode = head->link;
+	while(currentNode != head){
+		currentNode = currentNode->link;
+		cnt++;
+	}
 
 	return cnt;
 }
 
-
-
-
 // DO NOT REMOVE THE FOLLOWINGS. 
 template class CircularList<int>;
-
-
