64a17167097b5ca6648a097bade1a2710320b229
Francis Kim <kims_0121@naver.com>
2013-10-26 09:16:27 +0900

    lab006 완료

--- lab006/lab006.cpp
+++ lab006/lab006.cpp
@@ -1,6 +1,6 @@
 // Lab 006 
-// Name :
-// Student ID :
+// Name : ±è¼º±Ù
+// Student ID : 20093267
 
 #include "lab006.h"
 
@@ -10,10 +10,22 @@ void  MinHeap<T>::Pop()
 
 	if(heapSize == 0)
 		return;
+	
+	T last = heap[heapSize--];
 
+	int currentNode = 1;
+	int child = 2;
+	while(child <= heapSize){
 
+		if(child < heapSize && heap[child]>heap[child+1]) child++;
 
+		if(last <= heap[child]) break;
 
+		heap[currentNode] = heap[child];
+		currentNode = child;
+		child *= 2;
+	}
+	heap[currentNode] = last;
 }
 
 template <class T>
@@ -22,10 +34,9 @@ void  MinHeap<T>::PreOrder(const int idx)
 	if(idx > heapSize)
 		return;
 
-
-
-
-
+	cout << heap[idx] << ' ';
+	PreOrder(2*idx);
+	PreOrder(2*idx+1);
 
 }
 
