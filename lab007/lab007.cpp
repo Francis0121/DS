bc1323773ac251c08fd19c7f1ccf8751bf36ffba
Francis Kim <tjdrms0121@gmail.com>
2013-10-31 10:05:04 +0900

    lab 007 진행 Window 로 고고

--- lab007/lab007.cpp
+++ lab007/lab007.cpp
@@ -1,17 +1,31 @@
 // Lab 007 
-// Name :
-// Student ID :
+// Name : 김성근
+// Student ID : 20093267
 
 #include "lab007.h"
 
 
 template <class T>
 void  MaxLoserTree<T>::BuildLoserTree(const T *externalnode, int n)
-{   // initialize the array by using the input
+{   	
+	// initialize the array by using the input
 	// Then, compute the internal nodes
-
-    numofelements = n;
-
+	for(int i=0; i<=2n; i++){
+		li[i] = -1;
+	}
+    	
+	numofelements = n;
+	
+	for(int i=2*n-1; i>=0; i--){
+		if(n <= i){
+			li[i] = externalnode[i];
+		}else{
+			int left = li[2*i];
+			int right = li[2*i+1];
+			
+			li[i] = left >= right ? left : right;
+		}
+	}
 
 }
 
