d8cb927e049c9b1d4ef1464fd70cf56ca3562ce9
Francis Kim <kims_0121@naver.com>
2013-11-07 10:12:48 +0900

    lab008  완료?

--- lab008/lab008.cpp
+++ lab008/lab008.cpp
@@ -5,17 +5,26 @@
 #include "lab008.h"
 
 
-void Graph::Edge(int e1, int e2, int cost)
-{ 
-
-
+void Graph::Edge(int e1, int e2, int cost){ 
+	Cost[e1][e2] = cost;
+	Cost[e2][e1] = cost;
 }
 
-void Graph::Dfs(int v)
-{ 
+void Graph::Dfs(int v){ 
+	if(visited[v] == 1){
+		return;
+	}else{
+		visited[v] = 1;
+	}
 
-
-
+	for(int i=v; i<numofnodes; i++){
+		if(Cost[v][i] != 0){
+			if(visited[i] == 0){
+				cout << v << "-" << i << " : " << Cost[v][i] << endl;
+			}
+			Dfs(i);
+		}
+	}
 }
 
 
