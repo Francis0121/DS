9625743b5b6984aed5b207b4f52fe271b787ecc4
Francis Kim <kims_0121@naver.com>
2013-11-14 14:44:41 +0900

    lab009 완료?

--- lab009/lab009.cpp
+++ lab009/lab009.cpp
@@ -1,22 +1,47 @@
 // Lab 009 
-// Name :
-// Student ID :
+// Name : 
+// Student ID : 20093267
 
 #include "lab009.h"
 
-
-void Graph::Edge(int e1, int e2, int cost)
-{ 
-
+void Graph::Edge(int e1, int e2, int cost){ 
+	Cost[e1][e2] = cost;
 }
 
-void Graph::OutPath(int i)
-{ 
+void Graph::OutPath(int i){	
+	// p[i] i까지 가능 경로의 전 노드 임으로 recursive 하게 구하면된다.
+	if(p[i] == -1){
+		cout << i << " ";
+		return;
+	}
 
+	OutPath(p[i]);
+	cout << i << " ";
 }
 
-void Graph::BellmanFord(int v)
-{ 
+void Graph::BellmanFord(int v){ 
+	// Initialize d(*)
+	dist[v] = 0;
+	for(int i=0; i < numofnodes; i++){
+		if(i != v) dist[i] = LARGECOST;
+		p[i] = -1;
+	}
+	
+	for(int k=1; k < numofnodes; k++){
+
+		for(int n=0; n < numofnodes; n++){
+
+			for(int w=0; w < numofnodes; w++){
+				int cost = Cost[w][n];
+				if(dist[n] > dist[w] + cost){
+					dist[n] = dist[w]+cost;
+					p[n] = w;
+				}
+			}
+
+		}
+
+	}
 
 }
 
