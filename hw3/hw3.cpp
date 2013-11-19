ea34adccebd64619211f78061b7459ed13caffab
Francis Kim <kims_0121@naver.com>
2013-11-19 14:04:08 +0900

    Hw3

--- hw3/hw3.cpp
+++ hw3/hw3.cpp
@@ -1,21 +1,18 @@
 // HW 3
 // Name :
-// Student ID :
+// Student ID : 20093267
 
 #include "hw3.h"
 
-
 template <class T>
-void  QuickSort<T>::Swap(int i, int j)
-{
-
-
+void  QuickSort<T>::Swap(int i, int j){
+	int temp = arr[i];
+	arr[i] = arr[j];
+	arr[j] = temp;
 }
 
-
 template <class T>
-void  QuickSort<T>::QSort(int s, int e)
-{	
+void  QuickSort<T>::QSort(int s, int e){	
 	// sort arr[s:e] into nonincreasing order
 	cout << "Sort in [" << s << "," << e << "]" << endl;
 	Show(s, e);
@@ -24,16 +21,38 @@ void  QuickSort<T>::QSort(int s, int e)
 		return;
 
 	// Implement at the following....
+	int m = ( s + e ) / 2;
+	int leftmost = arr[s], middle = arr[m], rightmost = arr[e];
+	int p;
 
+	if( (leftmost <= middle && leftmost >= rightmost ) || (leftmost <= rightmost && leftmost >= middle ) ){
+		p = s;
+	}
 
+	if( (middle <= leftmost && middle >= rightmost ) || (middle <= rightmost && middle >= leftmost ) ){
+		p = m;
+	}
 
+	if( (rightmost <= middle && rightmost >= leftmost ) || (rightmost <= leftmost && rightmost >= middle ) ){
+		p = e;
+	}
+	
+	Swap(s, p);
 
+	if(s < e){
+		int i = s, j = e+1, pivot = arr[s];
+		do{
+			do i++; while(arr[i] > pivot);
+			do j--; while(arr[j] < pivot);
+			if(i<j) Swap(i, j);
+		}while(i<j);
+		Swap(s, j);
 
-
-
+		QSort(s, j-1);
+		QSort(j+1, e);
+	}
 
 }
-
 
 // DO NOT REMOVE THE FOLLOWING LINE
 template class QuickSort<int>;
